76357c4f0fbc0261fe12a87287e4a33266b5d023
ayush dubey <Ayush.dubey1@wipro.com>
2020-06-21 13:38:30 +0530

    sf

--- Aggregator/src/main/java/com/services/aggregator/model/AggregatorModel.java
+++
@@ -1,5 +0,0 @@
-package com.services.aggregator.model;
-
-public class AggregatorModel {
-
-}
--- Product/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into product (ID,CATEGORY,DESCRIPTION,NAME,PRICE) values(1,'Electronics',' BT Headset','Samsung BT',2000);
-insert into product (ID,CATEGORY,DESCRIPTION,NAME,PRICE) values(2,'Electronics','Stopwatch','Timesnow Watch',400);
-
-insert into product (ID,CATEGORY,DESCRIPTION,NAME,PRICE) values(3,'Sports','Hockey Stick','Xeno Stick',2000);
-insert into product (ID,CATEGORY,DESCRIPTION,NAME,PRICE) values(4,'Clothing','Nylon Jersey','Puma Jersey',900);
-
-
